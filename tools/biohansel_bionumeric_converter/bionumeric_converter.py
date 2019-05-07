063e682cb57bb223ab3485a9e07e0aa8830afcc0
Darian Hole <darianhole@gmail.com>
2019-05-07 13:25:21 -0500

    qc_message keeps up to 450 characters now

--- tools/biohansel_bionumeric_converter/bionumeric_converter.py
+++ tools/biohansel_bionumeric_converter/bionumeric_converter.py
@@ -14,7 +14,7 @@ def main():
         '-f',
         '--filename',
         required=True,
-        help='Specify your tsv input')
+        help='Specify your biohansel tsv or other tabular separated input')
     parser.add_argument(
         '-o',
         '--output',
@@ -24,30 +24,26 @@ def main():
     tsv_file = args.filename
     out_name = args.output
 
-    no_comma_tsv = comma_remover(tsv_file)
-    df = qc_shortener(no_comma_tsv)
+    df_input = pd.read_csv(tsv_file, sep='\t')
+
+    df_no_comma = df_input.replace(',', '/', regex=True)
+    df = qc_shortener(df_no_comma)
     df.to_csv(out_name, index=False)
-
-# Remove comma function:
-
-
-def comma_remover(tsv_file):
-    # Create a table from the tsv file as an input into the dataframe.
-    df = pd.read_csv(tsv_file, sep='\t')
-    # Change all commas to / in the QC message
-    no_comma_tsv = df.replace(',', '/', regex=True)
-    return no_comma_tsv
 
 # Shorten QC results:
 
 
 def qc_shortener(df):
-    for count in df.index:
-        message = str(df.at[count, 'qc_message'])
-        if len(message) > 150:
-            results = message.find('|')
-            new_message = "Truncated after first '|' : " + message[0:results]
-            df['qc_message'] = df['qc_message'].replace(message, new_message)
+    for i, row in df.iterrows():
+        message = row['qc_message']
+        try:
+            if len(message) > 150:
+                df.at[i,'qc_message'] = message[0:150]
+                df.at[i, 'qc_message_2'] = message[150:]
+                if len(message) > 300:
+                    df.at[i, 'qc_message_3'] = message[300:450]
+        except TypeError:
+            pass
     return df
 
 
