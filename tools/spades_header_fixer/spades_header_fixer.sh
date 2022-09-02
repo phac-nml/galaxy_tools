5843016817963a84096a31ff55f54d4cc00db15c
Connor Chato <ConnorChato@gmail.com>
2022-09-02 15:51:17 -0400

    re-fix command

--- tools/spades_header_fixer/spades_header_fixer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if grep -q ">NODE" $1; then
-    sed -r "s/>NODE(_[0-9]+)_(.*)/>$1\1 \2/g" $1 >$2
-fi
-if grep -q ">contig" $1; then
-    sed -r "s/>contig([0-9]+) (.*)/>$1\1 \2/g" $1 >$2
-fi
